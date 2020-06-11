a6552d9ea836d0287edf4660df2b95843fa206c0
amith2610 <amith2610@gmail.com>
2020-06-11 11:13:19 +0530

    updated

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 from resources.emp import Emp
+
 app=Flask(__name__)
-
 api=Api(app)
-
 
 api.add_resource(Emp,'/emp')
 
-app.run(port="8055",debug=True)
+if __name__=='__main__':
+    app.run()
--- db.py
+++ db.py
@@ -8,7 +8,6 @@ def query(querystr,return_json=True):
                                 password='coscskillup',
                                 db='testapi',
                                 cursorclass=pymysql.cursors.DictCursor )
-    
     connection.begin()
     cursor=connection.cursor()
     cursor.execute(querystr)
@@ -18,12 +17,12 @@ def query(querystr,return_json=True):
     connection.close()
     if return_json:
          return jsonify(result)
-    else: return result 
+    else:
+        return result
+
 def encode(data):
     for row in data:
         for key,value in row.items():
             if isinstance(value,Decimal):
                 row[key]=str(value)
     return data
-
-            
